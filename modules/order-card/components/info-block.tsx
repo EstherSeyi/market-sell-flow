503da0b9a03399e6b5ad066a2400416331022c6b
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-10-03 12:59:30 +0100

    updates info block

--- modules/order-card/components/info-block.tsx
+++ modules/order-card/components/info-block.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import Info from "../../../public/svgs/info.svg";
 
-const StyledInfoBlock = styled.div`
+const StyledInfoBlock = styled.div<{ secondary?: boolean }>`
   margin: 1.25em 0;
   .info-container {
     display: flex;
@@ -16,26 +16,43 @@ const StyledInfoBlock = styled.div`
     text-transform: uppercase;
     color: #292535;
     margin-right: 0.5em;
+    line-height: 16px;
+    letter-spacing: 0.1em;
   }
   .info-content {
     border: 1px solid #dcdae9;
     padding: 0.5em 0.25em;
     border-radius: 4px;
+    line-height: 22px;
+    color: #77757e;
+    font-weight: 300;
+
+    ${({ secondary }) => {
+      return (
+        secondary &&
+        css`
+          background-color: rgba(220, 218, 233, 0.3);
+          border-style: none;
+        `
+      );
+    }}
   }
 `;
 
 const InfoBlock = ({
   children,
   title,
+  secondary,
 }: {
   children: React.ReactNode;
   title: string;
+  secondary?: boolean;
 }) => {
   return (
-    <StyledInfoBlock>
+    <StyledInfoBlock secondary={secondary}>
       <div className="info-container">
         <h3 className="info-title">{title}</h3>
-        <Info className="info-icon" />
+        {!secondary && <Info className="info-icon" />}
       </div>
       <div className="info-content">{children}</div>
     </StyledInfoBlock>
