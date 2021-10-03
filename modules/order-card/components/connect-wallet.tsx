54bdb77da8dd30b3a57d962234363136126682cc
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-10-03 20:48:07 +0100

    updates connect wallet component

--- modules/order-card/components/connect-wallet.tsx
+++ modules/order-card/components/connect-wallet.tsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
 import Button from "../../../common/components/button";
 
-const ConnectWallet = () => {
+const ConnectWallet = ({ setCurrentAccount, setWalletIsConnected }: any) => {
+  const [connecting, setConnecting] = useState(false);
+  const handleConnectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Please get the Metamask browser extension");
+        return;
+      }
+      setConnecting(true);
+
+      const accounts = ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setCurrentAccount(accounts[0]);
+      setWalletIsConnected(true);
+      setConnecting(false);
+    } catch (error) {
+      setConnecting(false);
+      console.log(error);
+    }
+  };
+
   return (
-    <Button className="page-action__btn" color="#CD6116">
+    <Button
+      className="page-action__btn"
+      color="#CD6116"
+      onClick={handleConnectWallet}
+      isLoading={connecting}
+    >
       CONNECT TO METAMASK
     </Button>
   );
