901c8ecaf38992d03bd586ccd79f640a457e9424
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-10-03 13:00:55 +0100

    updates global styles

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -11,10 +11,54 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     font-family: Avenir;
   }
+  ul {
+list-style-type: none;}
+
   .flex-justify-between{
     display:flex;
     justify-content:space-between;
   }
+
+button,
+input{
+  font-family: inherit; 
+  font-size: 100%; 
+  line-height: 1.15; 
+  margin: 0;
+}
+
+button,
+input { 
+  overflow: visible;
+}
+
+button
+ { 
+  text-transform: none;
+  border-style: none;
+  cursor: pointer;
+}
+
+button,
+[type="button"],
+[type="reset"],
+[type="submit"] {
+  -webkit-appearance: button;
+}
+button::-moz-focus-inner,
+[type="button"]::-moz-focus-inner,
+[type="reset"]::-moz-focus-inner,
+[type="submit"]::-moz-focus-inner {
+  border-style: none;
+  padding: 0;
+}
+
+button:-moz-focusring,
+[type="button"]:-moz-focusring,
+[type="reset"]:-moz-focusring,
+[type="submit"]:-moz-focusring {
+  outline: 1px dotted ButtonText;
+}
 `;
 
 export default GlobalStyle;
