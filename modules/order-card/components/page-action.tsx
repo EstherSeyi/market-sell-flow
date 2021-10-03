201185678b2cdd13a0e09725abfcb7d32918c3c2
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-10-03 20:48:32 +0100

    updates page action component

--- modules/order-card/components/page-action.tsx
+++ modules/order-card/components/page-action.tsx
@@ -1,11 +1,52 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import ConnectWallet from "./connect-wallet";
 
 const PageAction = () => {
+  const [walletIsConnected, setWalletIsConnected] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState({});
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        setWalletIsConnected(true);
+        setCurrentAccount(accounts[0]);
+      } else {
+        setWalletIsConnected(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log({ currentAccount });
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <StyledContainer className="page-action__container">
-      <ConnectWallet />
+      {!walletIsConnected ? (
+        <ConnectWallet
+          setCurrentAccount={setCurrentAccount}
+          setWalletIsConnected={setWalletIsConnected}
+        />
+      ) : null}
     </StyledContainer>
   );
 };
