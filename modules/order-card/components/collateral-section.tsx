ef7071596984cb0a5a635f3afbd90e5f76d98582
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-10-03 12:59:09 +0100

    updates collateral section

--- modules/order-card/components/collateral-section.tsx
+++ modules/order-card/components/collateral-section.tsx
@@ -5,24 +5,45 @@ import Safe from "../../../public/svgs/safe.svg";
 import Slider from "../../../common/components/slider";
 
 const StyledSection = styled.div`
-  .collateral__header {
-    color: #49d273;
+  .collateral__header,
+  .collateral__header-safe {
+    display: flex;
   }
-  .collateral__safe-icon {
+  .collateral__header {
+    justify-content: space-between;
+    align-items: center;
+  }
+  .collateral__header-safe {
+    color: #49d273;
+    display: flex;
+    align-items: center;
+  }
+  .header-safe__icon {
     width: 16px;
     height: 16px;
+    margin-right: 0.25em;
+  }
+  .header-safe__text {
+    font-size: 0.75em;
+    align-self: flex-end;
+  }
+  .collateral__header-text {
+    font-size: 0.625rem;
+    color: #77757e;
+    font-weight: 500;
+    line-height: 14px;
   }
 `;
 
 const CollateralSlider = () => {
   return (
     <StyledSection>
-      <div className="flex-justify-between">
-        <div className="collateral__header">
-          <Safe className="collateral__safe-icon" />
-          <span className="collateral__header-safe">SAFE</span>
+      <div className="collateral__header">
+        <div className="collateral__header-safe">
+          <Safe className="header-safe__icon" />
+          <span className="header-safe__text">SAFE</span>
         </div>
-        <span>Collateral %</span>
+        <span className="collateral__header-text">Collateral %</span>
       </div>
       <Slider />
     </StyledSection>
