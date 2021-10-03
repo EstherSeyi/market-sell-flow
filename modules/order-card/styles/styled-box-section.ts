d6d748452c64f8d278e043975c0a119411ee9050
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-10-03 13:00:18 +0100

    adds styledsection box

---
+++ modules/order-card/styles/styled-box-section.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledSectionBox = styled.div`
+  box-shadow: 0px 4px 10px rgba(222, 222, 222, 0.5);
+  border-radius: 4px;
+  padding: 1em;
+  margin-bottom: 1em;
+  width: 95%;
+  margin: 0 auto;
+`;
