b2d3cf3b426a1b8a921b05806210534234a7db44
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-10-03 13:00:43 +0100

    updates home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,116 +1,202 @@
 import type { NextPage } from "next";
 import styled from "styled-components";
 
-import Button from "../common/components/button/button";
+import Button from "../common/components/button";
 import InfoBlock from "../modules/order-card/components/info-block";
 import CollateralSlider from "../modules/order-card/components/collateral-section";
+import TxAction from "../modules/order-card/components/tx-action";
+import TxSummary from "../modules/order-card/components/tx-summary";
+import DescriptionSection from "../modules/order-card/components/description-section";
+import ValueAdjuster from "../common/components/value-adjuster";
 
-import PutIcon from "../public/svgs/alt-inverted-arrows.svg";
-import Info from "../public/svgs/info.svg";
+import { StyledSectionBox } from "../modules/order-card/styles/styled-box-section";
+
+const Home: NextPage = () => {
+  return (
+    <Main>
+      <DescriptionSection />
+      <StyledSectionBox>
+        <div className="market-limit-tabs">
+          <button className="market-tab">Market</button>
+          <button className="limit-tab">Limit</button>
+        </div>
+        <div>
+          <InfoBlock title="position size">
+            <div className="position-size__otoken-section">
+              <span className="position-size__otoken-text">
+                oToken Balance:{" "}
+              </span>
+              <span className="position-size__otoken-bal">21.042</span>
+            </div>
+            <div className="position-size__max-otoken">
+              <span className="position-size__max-otoken-value">100.00</span>
+              <div className="position-size__max-otoken-text">
+                <span className="position-size__max">MAX</span>
+                <span>oTokens</span>
+              </div>
+            </div>
+          </InfoBlock>
+
+          <InfoBlock title="collateralization ratio">
+            <div className="collateralization-ratio">
+              <span className="collateralization-ratio__value">25</span>
+              <span className="collateralization-ratio__percent">%</span>
+            </div>
+          </InfoBlock>
+          <InfoBlock title="spot change">
+            <div className="spot-change">
+              <ValueAdjuster />
+
+              <div className="spot-change__value-container">
+                <span className="spot-change__value">25</span>
+                <span className="spot-change__percent">%</span>
+              </div>
+            </div>
+          </InfoBlock>
+          <CollateralSlider />
+
+          <InfoBlock secondary title="Liquidation price">
+            <span>~$146.79</span>
+          </InfoBlock>
+          <InfoBlock secondary title="Collateral">
+            <span>$100.00</span>
+          </InfoBlock>
+          <TxAction />
+          <TxSummary />
+        </div>
+      </StyledSectionBox>
+      <div className="page-action__container">
+        <Button className="page-action__btn" color="#CD6116">
+          CONNECT TO METAMASK
+        </Button>
+      </div>
+    </Main>
+  );
+};
 
 const Main = styled.main`
-  width: 95%;
+  border: 1px solid rgba(220, 218, 233, 0.2);
   max-width: 400px;
   margin: 0 auto;
-  padding: 1em;
-  margin-top: 1em;
-
-  .topic-container {
-    display: flex;
-    align-items: center;
-  }
-  .topic {
-    font-weight: 500;
-    margin-right: 0.5em;
-    font-size: 1.5rem;
-  }
 
   .info-icon {
-    color: #dcdae9;
+    color: rgba(119, 117, 126, 0.5);
     width: 14px;
     height: 14px;
   }
 
-  .section-box-style {
+  .page-action__container {
     box-shadow: 0px 4px 10px rgba(222, 222, 222, 0.5);
-    border-radius: 4px;
+    margin-top: 1em;
     padding: 1em;
-    margin-bottom: 1em;
+  }
+  .page-action__btn {
+    width: 100%;
+  }
+
+  .market-limit-tabs {
+    display: flex;
+  }
+  .market-tab,
+  .limit-tab {
+    flex-basis: 50%;
+    text-align: center;
+    padding: 1em 0;
+    text-transform: uppercase;
+    font-size: 0.75rem;
+    letter-spacing: 0.1em;
+    line-height: 16px;
+    font-weight: 300;
+    cursor: pointer;
+
+    background: rgba(220, 218, 233, 0.2);
+    border-bottom: 1px solid rgba(41, 37, 53, 0.2);
+    &:focus,
+    &:active {
+      background: transparent;
+      border-style: none;
+    }
+    &:hover {
+      font-weight: 500;
+    }
+  }
+
+  .market-tab {
+    border-right: 1px solid rgba(41, 37, 53, 0.2);
+  }
+  .limit-tab {
+    border-left: 1px solid rgba(41, 37, 53, 0.2);
+  }
+
+  .position-size__otoken-section {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: rgba(220, 218, 233, 0.3);
+    padding: 0.25em;
+    border-radius: 2px;
+    color: #77757e;
+    margin-bottom: 0.6875em;
+  }
+  .position-size__otoken-text {
+    font-size: 0.625rem;
+    line-height: 14px;
+  }
+  .position-size__otoken-bal {
+    font-size: 0.75rem;
+    font-weight: 800;
+    line-height: 12px;
+  }
+
+  .position-size__max-otoken {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 0.5em;
+  }
+
+  .position-size__max-otoken-value {
+    font-size: 20px;
+    line-height: 20px;
+  }
+  .position-size__max-otoken-text {
+    font-size: 0.625rem;
+  }
+  .position-size__max {
+    font-size: 0.5rem;
+    padding: 0.25em;
+    color: #4dadf3;
+    border: 1px solid #4dadf3;
+    border-radius: 4px;
+    margin-right: 0.5em;
+  }
+  .collateralization-ratio,
+  .spot-change {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 0.5em;
+    align-items: center;
+  }
+
+  .collateralization-ratio__value,
+  .spot-change__value {
+    color: #292535;
+    font-size: 1rem;
+    font-weight: 300;
+    line-height: 22px;
+  }
+  .collateralization-ratio__percent,
+  .spot-change__percent {
+    font-size: 0.75rem;
+    line-height: 16px;
+    color: #77757e;
+  }
+  .spot-change__value-container {
+    display: flex;
+    align-items: center;
+  }
+  .spot-change__value {
+    margin-right: 0.5em;
   }
 `;
-
-const Home: NextPage = () => {
-  return (
-    <>
-      <Main>
-        <div className="section-box-style">
-          <div className="topic-container">
-            <h1 className="topic">Order Card</h1>
-            <Info className="info-icon" />
-          </div>
-          <div>
-            <Button>Buy</Button>
-            <Button>Sell</Button>
-            <div>
-              <span>WETH</span>
-              <span>FRI, 30 APR 2021</span>
-            </div>
-            <div>
-              <PutIcon />
-              <span>PUT</span>
-              <span>--</span>
-              <span>$2080 STRIKE</span>
-            </div>
-            <div>
-              <Info className="info-icon" />
-              <div>
-                To create a spread, select another option.
-                <a href="#">Learn more</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="section-box-style">
-          <div>
-            <p>Market</p>
-            <p>Limit</p>
-          </div>
-          <div>
-            <InfoBlock title="position size">
-              <div className="flex-justify-between">
-                <span>oToken Balance: </span>
-                <span>21.042</span>
-              </div>
-              <div className="flex-justify-between">
-                <span>100.00</span>
-                <div>
-                  <span>MAX</span>
-                  <span>oTokens</span>
-                </div>
-              </div>
-            </InfoBlock>
-
-            <InfoBlock title="collateralization ratio">
-              <div className="flex-justify-between">
-                <span>25</span>
-                <span>%</span>
-              </div>
-            </InfoBlock>
-            <InfoBlock title="spot change">
-              <div className="flex-justify-between">
-                <div>
-                  <span>-</span>
-                  <span>+</span>
-                </div>
-                <span>25</span>
-              </div>
-            </InfoBlock>
-            <CollateralSlider />
-          </div>
-        </div>
-      </Main>
-    </>
-  );
-};
 
 export default Home;
