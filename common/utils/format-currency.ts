bc8009bd15ada4bacb91174c89a4c12d003c004d
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-10-04 20:54:57 +0100

    adds util to format currency

---
+++ common/utils/format-currency.ts
@@ -0,0 +1,10 @@
+// USD Formats
+const USDCurrencyFormat = {
+  style: "currency",
+  currency: "USD",
+};
+
+export const i18nUSDCurrencyFormat = new Intl.NumberFormat(
+  "en-US",
+  USDCurrencyFormat
+);
