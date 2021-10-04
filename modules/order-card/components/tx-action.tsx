c5adf95738693e5e522d733d3cf8b4dfe1ff9cfc
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-10-04 17:12:13 +0100

    update tx-action

--- modules/order-card/components/tx-action.tsx
+++ modules/order-card/components/tx-action.tsx
@@ -1,19 +1,18 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import Bullet from "../../../public/svgs/bullet.svg";
 import Info from "../../../public/svgs/info.svg";
+import Check from "../../../public/svgs/check-circle.svg";
+
+import { useStep } from "../custom-hooks/use-step";
 
 const Styled = styled.div`
   margin: 1em 0 1.5em 0;
   .txaction__icon {
     width: 16px;
     height: 16px;
-    color: #292535;
+    color: #77757e;
     margin-right: 0.25em;
-  }
-  .txaction__icon--info {
-    color: rgba(119, 117, 126, 0.5);
-    margin-right: 0;
   }
   .txaction__icon--info--disabled {
     color: #77757e;
@@ -35,45 +34,74 @@ const Styled = styled.div`
     color: #292535;
   }
   .txaction__item-text {
-    flex-basis: 80%;
-    font-weight: 500;
-    font-size: 0.75rem;
-    line-height: 14px;
-    color: #292535;
-  }
-  .txaction__item-text--current {
-    text-decoration: underline;
-  }
-  .txaction__item-text--disabled {
-    font-weight: 300;
-    color: #77757e;
-  }
-  .txaction__item-text {
     font-family: Roboto;
   }
 `;
+const StyledItem = styled.li<{ isCurrent: boolean }>`
+  .txaction__item-text {
+    flex-basis: 80%;
+    font-weight: 300;
+    font-size: 0.75rem;
+    line-height: 14px;
+    font-weight: 300;
+    color: #77757e;
+
+    ${({ isCurrent }) =>
+      isCurrent &&
+      css`
+        text-decoration: underline;
+        color: #292535;
+        font-weight: 800;
+      `}
+  }
+
+  .txaction__icon {
+    ${({ isCurrent }) =>
+      isCurrent &&
+      css`
+        color: #292535;
+      `}
+  }
+  .txaction__icon--info {
+    color: rgba(119, 117, 126, 0.5);
+    margin-right: 0;
+  }
+`;
+
+const steps = [
+  "1. Enable WETH Wrapper",
+  "2. Approve collateral to Opyn Contracts",
+  "3. Permit oToken to wrapper contracts",
+  "4. Permit, deposit, mint & trade",
+];
 
 const TxAction = () => {
   return (
     <Styled>
       <h3 className="tx-action__title">Tx Action</h3>
       <ul>
-        <TxActionItem text="1. Enable WETH Wrapper" />
-        <TxActionItem text="2. Approve collateral to Opyn Contracts" />
-        <TxActionItem text="3. Permit oToken to wrapper contracts" />
-        <TxActionItem text="4. Permit, deposit, mint & trade" />
+        {steps.map((item, index) => (
+          <TxActionItem text={item} key={item} step={index + 1} />
+        ))}
       </ul>
     </Styled>
   );
 };
 
-const TxActionItem = ({ text }: { text: string }) => {
+const TxActionItem = ({ text, step }: { text: string; step: number }) => {
+  const { currentStep } = useStep();
   return (
-    <li className="txaction__item ">
-      <Bullet className="txaction__icon" />
+    <StyledItem isCurrent={step === currentStep} className="txaction__item ">
+      {step === 4 ? (
+        <Check className="txaction__icon" />
+      ) : (
+        <Bullet className="txaction__icon" />
+      )}
+
       <span className="txaction__item-text">{text}</span>
+
       <Info className="txaction__icon txaction__icon--info" />
-    </li>
+    </StyledItem>
   );
 };
 
