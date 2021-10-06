91b6295aa85eed5442dbce000d8727453c08b304
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-10-06 03:40:24 +0100

    fixes date bug

--- modules/order-card/utils/format-expiry-date.ts
+++ modules/order-card/utils/format-expiry-date.ts
@@ -1,7 +1,8 @@
 import format from "date-fns/format";
 
 export default function formatExpiryDate(dateString = "") {
-  if (!dateString || dateString == "Invalid date") return "N/A";
+  if (!dateString || typeof Date.parse(dateString) !== "number") return "N/A";
+
   const date = new Date(dateString);
 
   return format(date, "E, MM LLL yyyy");
